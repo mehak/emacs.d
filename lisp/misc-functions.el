c50bb3810d93835dd456e95c9ba9a6fdec0a526d
Nathanael Merlin <nate@merlin.uno>
2019-05-10 10:52:08 -0700

    Remove trailing whitespace
    Signed-off-by: Nathanael Merlin <nate@merlin.uno>

--- lisp/misc-functions.el
+++ lisp/misc-functions.el
@@ -33,7 +33,7 @@
 (defun lines-to-cslist (start end &optional arg)
   (interactive "r\nP")
   (let ((insertion
-         (mapconcat 
+         (mapconcat
           (lambda (x) (format "\"%s\"" x))
           (split-string (buffer-substring start end)) ", ")))
     (delete-region start end)
