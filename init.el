01e2db4bfce3e3efe9eecba64af93a613e5ef007
Nathanael Merlin <nate@merlin.uno>
2019-05-16 15:29:14 -0700

    Add term-mode to no emojify
    Signed-off-by: Nathanael Merlin <nate@merlin.uno>

--- init.el
+++ init.el
@@ -364,7 +364,7 @@ Null prefix argument turns off the mode."
 (load-theme 'sanityinc-tomorrow-night t)
 
 ;; NO EMOJIS HERE!!!!
-(setq emojify-inhibit-major-modes '(dired-mode doc-view-mode debugger-mode pdf-view-mode image-mode help-mode ibuffer-mode magit-popup-mode magit-diff-mode ert-results-mode compilation-mode proced-mode mu4e-headers-mode eshell-mode))
+(setq emojify-inhibit-major-modes '(dired-mode doc-view-mode debugger-mode pdf-view-mode image-mode help-mode ibuffer-mode magit-popup-mode magit-diff-mode ert-results-mode compilation-mode proced-mode mu4e-headers-mode eshell-mode term-mode))
 ;; Emojis everywhere else
 (add-hook 'after-init-hook #'global-emojify-mode)
 
