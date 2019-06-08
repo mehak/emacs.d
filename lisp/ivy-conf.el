70fa668a5e43fb547b4441c58101292df52f3c73
Nathanael Merlin <nate@merlin.uno>
2019-06-08 01:18:42 -0700

    Add EXWM/Major mode column for ivy-switch-buffer
    This matches the EXWM-mode column in ibuffer
    
    Signed-off-by: Nathanael Merlin <nate@merlin.uno>

--- lisp/ivy-conf.el
+++ lisp/ivy-conf.el
@@ -27,3 +27,37 @@
 (global-set-key "\C-hf" 'counsel-describe-function)
 ;; Rebind C-h v to counsel-describe-variable
 (global-set-key "\C-hv" 'counsel-describe-variable)
+
+;; Show EXWM class or major mode
+(defun mehak/ivy-rich-switch-buffer-major-mode (candidate)
+  (with-current-buffer
+      (get-buffer candidate)
+    (mehak/major-mode-or-class)))
+
+(setq ivy-rich-display-transformers-list
+      '(ivy-switch-buffer
+        (:columns
+         ((ivy-rich-candidate (:width 50))
+          (ivy-rich-switch-buffer-size (:width 7))
+          (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right))
+          (mehak/ivy-rich-switch-buffer-major-mode (:width 12 :face warning))
+          (ivy-rich-switch-buffer-project (:width 15 :face success))
+          (ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3))))))
+         :predicate
+         (lambda (cand) (get-buffer cand)))
+        counsel-M-x
+        (:columns
+         ((counsel-M-x-transformer (:width 40))
+          (ivy-rich-counsel-function-docstring (:face font-lock-doc-face))))
+        counsel-describe-function
+        (:columns
+         ((counsel-describe-function-transformer (:width 40))
+          (ivy-rich-counsel-function-docstring (:face font-lock-doc-face))))
+        counsel-describe-variable
+        (:columns
+         ((counsel-describe-variable-transformer (:width 40))
+          (ivy-rich-counsel-variable-docstring (:face font-lock-doc-face))))
+        counsel-recentf
+        (:columns
+         ((ivy-rich-candidate (:width 0.8))
+          (ivy-rich-file-last-modified-time (:face font-lock-comment-face))))))
