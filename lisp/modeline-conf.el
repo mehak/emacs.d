8eaa312bc01eaf88a839f40c51690864a9528b0b
Nathanael Merlin <nate@merlin.uno>
2019-06-08 13:03:16 -0700

    Colorize more of the mode-line
    Signed-off-by: Nathanael Merlin <nate@merlin.uno>

--- lisp/modeline-conf.el
+++ lisp/modeline-conf.el
@@ -1,26 +1,75 @@
 ;;; -*- lexical-binding: t; -*-
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Setup a ruler (vim) like display at bottom
 (line-number-mode 1)
 (column-number-mode 1)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Minions - minor mode menus
 (use-package minions
   :config
   (minions-mode 1))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Battery mode
+(setq battery-mode-line-format "%p%%")
 (display-battery-mode 1)
 
+;; Pulled from smart-mode-line
+(defface sml/global           '((t :inverse-video nil)) "")
+(defface sml/charging            '((t :inherit sml/global :foreground "ForestGreen")) "")
+(defface sml/discharging         '((t :inherit sml/global :foreground "Red"))         "")
+
+(defun sml/set-battery-font ()
+  "Set `sml/battery' face depending on battery state."
+  (let ((data (and (boundp 'battery-status-function)
+                   battery-status-function
+                   (funcall battery-status-function))))
+    (if  (string-equal "AC" (cdr (assoc 76 data)))
+        (copy-face 'sml/charging 'sml/battery)
+      (copy-face 'sml/discharging 'sml/battery))))
+
+(defadvice battery-update (before sml/set-battery-font activate)
+  "Fontify the battery display."
+  (sml/set-battery-font))
+
+(eval-after-load 'battery
+  '(defadvice battery-update (after sml/after-battery-update-advice () activate)
+     "Change battery color."
+     (when battery-mode-line-string
+       (setq battery-mode-line-string
+             (propertize battery-mode-line-string
+                         'face 'sml/battery)))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Show the time
 (setq display-time-string-forms
-      '((format-time-string " [%F %R] ")))
+      '((propertize (format-time-string " %F %R")
+                    'face `(:foreground "dim grey"))))
 (display-time-mode 1)
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Line:Column
 (setq mehak-mode-line-position
       '(line-number-mode
         ("%l"
-         (column-number-mode " %C"))))
+         (column-number-mode ":%C"))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; default: #b294bb
+(modify-face 'mode-line-buffer-id "goldenrod")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 (setq-default
  mode-line-format
@@ -30,7 +79,6 @@
    " "
    mode-line-modified
    " "
-   evil-mode-line-tag
    minions-mode-line-modes
    mode-line-misc-info
    mode-line-buffer-identification
