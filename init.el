95888edf32cd35adeea99a77c5bee1859f1370f9
Nathanael Merlin <nate@merlin.uno>
2019-06-28 09:41:57 -0700

    Move encryption config to a separate file
    Signed-off-by: Nathanael Merlin <nate@merlin.uno>

--- init.el
+++ init.el
@@ -57,44 +57,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-;;;;;;;;;;;;;;;;;;;;;;;; Encryption ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Easy PG (GPG)
-(require 'epa-file)
-(epa-file-enable)
-
-
-(define-minor-mode sensitive-mode
-  "For sensitive files like password lists.
-It disables backup creation and auto saving.
-
-With no argument, this command toggles the mode.
-Non-null prefix argument turns on the mode.
-Null prefix argument turns off the mode."
-  ;; The initial value
-  nil
-  ;; The indicator for the mode line
-  " Sensitive"
-  ;; The minor mode bindings
-  nil
-  (if (symbol-value sensitive-mode)
-      (progn
-        ;; disable backups
-        (set (make-local-variable 'backup-inhibited) t)
-        ;; disable auto-save
-        (if auto-save-default
-            (auto-save-mode -1)))
-    ;; resort to default value of backup-inhibited
-    (kill-local-variable 'backup-inhibited)
-    ;; resort to default auto save setting
-    (if auto-save-default
-        (auto-save-mode 1))))
-
-(setq auto-mode-alist
-      (append '(("\\.gpg$" . sensitive-mode))
-              auto-mode-alist))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
 ;;;;;;;;;;;;;;;;;;;;;;;; Magit ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Show differences for specific hunk only
 (setq-default magit-diff-refine-hunk t)
