13edca46feb1bb241873ca320cfd066386a2f921
Nathanael Merlin <nate@merlin.uno>
2019-06-28 09:37:41 -0700

    Move term-mode config into its own file
    Signed-off-by: Nathanael Merlin <nate@merlin.uno>

---
+++ lisp/term-conf.el
@@ -0,0 +1,17 @@
+;;; -*- lexical-binding: t; -*-
+
+(require 'term)
+
+;; Helpful keybindings for term-mode
+;; http://joelmccracken.github.io/entries/switching-between-term-mode-and-line-mode-in-emacs-term/
+(defun jnm/term-toggle-mode ()
+  "Toggle between line mode and char mode"
+  (interactive)
+  (if (term-in-line-mode)
+      (term-char-mode)
+    (term-line-mode)))
+
+(define-key term-mode-map (kbd "C-c C-j") 'jnm/term-toggle-mode)
+(define-key term-raw-map (kbd "C-c C-j") 'jnm/term-toggle-mode)
+(define-key term-mode-map (kbd "C-c C-k") 'jnm/term-toggle-mode)
+(define-key term-raw-map (kbd "C-c C-k") 'jnm/term-toggle-mode)
