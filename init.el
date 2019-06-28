c7e79ef4d7d2f14348b9d6580fb23d83e6720db5
Nathanael Merlin <nate@merlin.uno>
2019-06-28 09:43:17 -0700

    Move erc config to separate file
    Signed-off-by: Nathanael Merlin <nate@merlin.uno>

--- init.el
+++ init.el
@@ -127,22 +127,6 @@
 (pdf-loader-install)
 
 
-;;;;;;;;;;;;;;;;;;;;;;;; erc ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; erc
-(setq
- erc-log-channels-directory "~/.erc/logs/"
- erc-save-buffer-on-part nil
- erc-save-queries-on-quit nil
- erc-log-write-after-send t
- erc-log-write-after-insert t
- erc-rename-buffers t
- erc-nick "mehak"
- erc-server "i.b1n.win"
- erc-port 6697
- erc-fill-column 80)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
 ;; tramp
 (setq tramp-default-method "ssh")
 
---
+++ lisp/erc-conf.el
@@ -0,0 +1,13 @@
+;;; -*- lexical-binding: t; -*-
+
+(setq
+ erc-log-channels-directory "~/.erc/logs/"
+ erc-save-buffer-on-part nil
+ erc-save-queries-on-quit nil
+ erc-log-write-after-send t
+ erc-log-write-after-insert t
+ erc-rename-buffers t
+ erc-nick "mehak"
+ erc-server "i.b1n.win"
+ erc-port 6697
+ erc-fill-column 80)
