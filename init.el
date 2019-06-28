beed27a1071d2116f9fc2410182f74e9ea0091ed
Nathanael Merlin <nate@merlin.uno>
2019-06-28 09:35:47 -0700

    Move trailing whitespace config to its own file
    Signed-off-by: Nathanael Merlin <nate@merlin.uno>

--- init.el
+++ init.el
@@ -40,27 +40,6 @@
 (set-face-attribute 'default nil :font "Source Code Pro" :height 83)
 
 
-;;;;;;;;;;;;;;;;;;;;;;;; Trailing whitespace ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Show trailing whitespace and remove it easily
-(setq-default show-trailing-whitespace t)
-(global-set-key (kbd "<f12>") 'delete-trailing-whitespace)
-
-;; https://emacs.stackexchange.com/a/40649
-(defun hide-trailing-whitespace-for-modes ()
-  "Disable `show-trailing-whitespace' in selected modes."
-  (when (or (derived-mode-p
-             'shell-mode
-             'erc-mode
-             'eshell-mode
-             'term-mode)
-            (minibufferp))
-    (setq show-trailing-whitespace nil)))
-
-(add-hook 'after-change-major-mode-hook
-          'hide-trailing-whitespace-for-modes)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
 ;;;;;;;;;;;;;;;;;;;;;;;; Evil ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Setup evil, evil-leader, and evil-collection
 (use-package evil
---
+++ lisp/trailing-whitespace-conf.el
@@ -0,0 +1,19 @@
+;;; -*- lexical-binding: t; -*-
+
+;; Show trailing whitespace and remove it easily
+(setq-default show-trailing-whitespace t)
+(global-set-key (kbd "<f12>") 'delete-trailing-whitespace)
+
+;; https://emacs.stackexchange.com/a/40649
+(defun hide-trailing-whitespace-for-modes ()
+  "Disable `show-trailing-whitespace' in selected modes."
+  (when (or (derived-mode-p
+             'shell-mode
+             'erc-mode
+             'eshell-mode
+             'term-mode)
+            (minibufferp))
+    (setq show-trailing-whitespace nil)))
+
+(add-hook 'after-change-major-mode-hook
+          'hide-trailing-whitespace-for-modes)
