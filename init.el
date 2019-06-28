e19505a434824bbce4d2697987d3ae730cb2e81a
Nathanael Merlin <nate@merlin.uno>
2019-06-28 09:34:19 -0700

    Remove ensure-package-installed
    This would break EXWM sometimes if a package wasn't installed
    
    Signed-off-by: Nathanael Merlin <nate@merlin.uno>

--- init.el
+++ init.el
@@ -25,33 +25,11 @@
 
 (setq package-enable-at-startup nil)
 
-;; TODO re-write to use cl-loop or dolist
-(defun ensure-package-installed (&rest packages)
-  "Assure everypackage is installed, ask for installing if it's not.
-
-Return a list of installed packages or nil for every skipped package."
-  (mapcar
-   (lambda (package)
-     (if (package-installed-p package)
-         nil
-       (if (y-or-n-p (format "Package %s is missing.  Install it? " package))
-           (package-install package)
-         package)))
-   packages))
-
 ;; Make sure to have downloaded archive description.
 (or (file-exists-p package-user-dir)
     (package-refresh-contents))
 
 (package-initialize)
-
-(ensure-package-installed 'evil
-                          'magit
-                          'evil-magit
-                          'evil-leader
-                          'use-package
-                          'counsel
-                          'yasnippet)
 
 (autoload 'dired-async-mode "dired-async.el" nil t)
 (dired-async-mode 1)
