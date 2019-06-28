87e47b01bac22af6799e129332f1d4f9fa249007
Nathanael Merlin <nate@merlin.uno>
2019-06-28 14:35:41 -0700

    Add keybinding for quiting the window
    Signed-off-by: Nathanael Merlin <nate@merlin.uno>

--- lisp/sdcv-conf.el
+++ lisp/sdcv-conf.el
@@ -5,3 +5,6 @@
 (setq sdcv-word-pronounce nil)
 (global-set-key (kbd "<f11>") 'sdcv-search-input)
 (global-set-key (kbd "<f10>") 'sdcv-search-pointer)
+
+(evil-define-key 'normal sdcv-mode-map
+  "q" 'quit-window)
