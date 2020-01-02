c1b676299bf20f8ccff0f53e9476bf484807fbba
Nathanael Merlin <nate@merlin.uno>
2020-01-02 12:03:57 -0800

    Add config to display buffer in same window
    Signed-off-by: Nathanael Merlin <nate@merlin.uno>

--- lisp/magit-conf.el
+++ lisp/magit-conf.el
@@ -2,3 +2,6 @@
 
 ;; Show differences for specific hunk only
 (setq-default magit-diff-refine-hunk t)
+
+;; Switch to magit-status buffer instead of splitting the window/frame
+(setq magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)
