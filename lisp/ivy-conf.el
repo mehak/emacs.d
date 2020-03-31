26d8c13f6074d2678fc1ea529c454644134d31fa
Nathanael Merlin <nate@merlin.uno>
2020-03-31 09:42:37 -0700

    Clear initial input for counsel-M-x
    Signed-off-by: Nathanael Merlin <nate@merlin.uno>

--- lisp/ivy-conf.el
+++ lisp/ivy-conf.el
@@ -9,7 +9,7 @@
       '((org-refile . "^")
         (org-agenda-refile . "^")
         (org-capture-refile . "^")
-        (counsel-M-x . "^")
+        (counsel-M-x . "")
         (counsel-describe-function . "^")
         (counsel-describe-variable . "^")
         (counsel-org-capture . "^")
