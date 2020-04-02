5a906beaa11537b974c8b65c73753f78983b9666
Nathanael Merlin <nate@merlin.uno>
2020-04-02 15:38:06 -0700

    Refactor to allow for generalization
    Signed-off-by: Nathanael Merlin <nate@merlin.uno>

--- lisp/firewall-functions.el
+++ lisp/firewall-functions.el
@@ -3,9 +3,8 @@
 ;; Collection of functions for working with firewalls in an easy way
 
 ;; TODO re-factor to make more clear
-(defun get-firewall-conf (firewall configuration-file)
-  "Connect to firewall, grab configuration and copy it to configuration-file"
-  (interactive "sFirewall IP: \nFConfiguration File: ")
+(defun get-firewall-conf (firewall configuration-file configuration-commands)
+  "Connect to a firewall, grab the configuration and copy it to configuration-file"
   (require 'term)
   (let* ((cmd "/usr/bin/ssh")
          (args firewall)
@@ -34,12 +33,20 @@
                (kill-region (- (point) 1)  (point-max)))))))
     (set-buffer termbuf)
     (term-mode)
-    (comint-send-string
-     process
-     (concat
-      "set cli pager off\n"
-      "set cli config-output-format set\n"
-      "configure\n"
-      "show\n"
-      "exit\n"
-      "exit\n"))))
+    (comint-send-string process configuration-commands)))
+
+;; Used to get the configuration from a palo alto firewall
+(defun get-pan-conf (firewall configuration-file)
+  "Connect to a palo-alto firewall,
+grab the configuration and copy it to configuration-file"
+  (interactive "sFirewall IP: \nFConfiguration File: ")
+  (get-firewall-conf
+   firewall
+   configuration-file
+   (concat
+    "set cli pager off\n"
+    "set cli config-output-format set\n"
+    "configure\n"
+    "show\n"
+    "exit\n"
+    "exit\n")))
